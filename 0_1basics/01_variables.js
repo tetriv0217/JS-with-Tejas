390236ed29427b33326e740cb01b03e0c828e7af
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-05-30 18:29:10 +0000

    Day1:- studied datatypes and variables

---
+++ 0_1basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14456
+
+let accountPassword = "1234567890";
+
+accountCity = "Bengaluru"
+// Not a good Practice
+let accountEmail = "tetriv0217@gmail.com"
+let accountState;
+//Undefined
+/* 
+    Dont use var ever again
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 0_1basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all js as newer version
+
+// alert(3+3); //We are using nodejs not browser
+
+let name = "Hitehs"
+let age = 15
+let isLoggedIn = false
+
+// number
+// String
+// boolean :- true, false
+// null => stand alone value
+// undefined => value not assigned
+
+// symbol :- It is used in react this is basically used for defining uniquenes.
+// object
+
+// console.log(typeof "hti");
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined
+
