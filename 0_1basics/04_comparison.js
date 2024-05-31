5ecd4654da7ea4e281f281d3bc28296c90ba7ef5
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-05-31 18:47:48 +0000

    Comapariosn done

---
+++ 0_1basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(1 > 2)
+// console.log(1 < 2)
+// console.log(1 >= 2)
+// console.log(1 <= 2)
+// console.log(1 == 2)
+// console.log(1 != 2)
+
+// console.log("2" > 1);//true
+// console.log("02" > 1);//true
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true;
+// conclusion :-  compare(>,<) works differently than ==
+
+// for undefined all would be false;
+
+// strict comparitor === 
+
+// console.log(2 === "2")
+// false as it checks datatype as well
