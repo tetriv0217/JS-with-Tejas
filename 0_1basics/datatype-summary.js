1e90d2519cffa4952e06fd4adb1fdd46d3375282
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-05-31 19:29:12 +0000

    Heap and Stack is explained

--- 0_1basics/datatype-summary.js
+++ 0_1basics/datatype-summary.js
@@ -31,9 +31,40 @@ const myFunction = function (){
     console.log("Hello World")
 }
 
-console.log(typeof myFunction);//function = Function object.
-console.log(typeof myObj)//object  = object
-console.log(typeof null)//null = object 
-console.log(typeof heros)//array = object
-console.log(typeof id1)//symbol = symbol
+// console.log(typeof myFunction);//function = Function object.
+// console.log(typeof myObj)//object  = object
+// console.log(typeof null)//null = object 
+// console.log(typeof heros)//array = object
+// console.log(typeof id1)//symbol = symbol
+
+
+// -------------------------------------------------------------------------
+
+// Stack(Primitive) :- We get copy of a variable
+// Heap (Non-Primitive) :- We get reference and Not the copy
+
+// Example of Stack
+let a = "Tejas"
+let b = a
+b = "Archie"
+console.log(a);//Tejas
+console.log(b);//Archie
+//In stack the value is not taken as reference but a copy so "b" changed value doesnt affect a
+
+//Heap Example
+
+let user1 = {
+    email:"tetriv0217@gmail.com",
+    age:22,
+}
+let user2 = user1;
+
+user2.email = "21103250@mail.jiit.ac.in"
+
+console.log(user1.email);//21103250@mail.jiit.ac.in
+console.log(user2.email);//21103250@mail.jiit.ac.in
+
+//Explanation :- //Here the value changes to newEmail,since it is in heap and the values in heap are taken as reference and not as a copy.
+
+
 
