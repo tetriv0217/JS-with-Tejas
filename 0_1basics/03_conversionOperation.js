2bc91b3db27a82991427fc9e07ad3a01dc971e20
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-05-31 18:12:23 +0000

    Done conversion

---
+++ 0_1basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let a = "33abc";
+let a = undefined;
+let convertAtoNumber = Number(a);
+// console.log(typeof(a));
+// console.log(typeof(convertAtoNumber));
+// console.log(convertAtoNumber);//NaN
+
+// Notes
+// "33" => 33
+// "33abc" => NaN /Not a Number/
+// null => 0
+// true => 1;false => 0
+// undefined Nan
+
+// boolean coversion
+let b = "asds";
+let bbool = Boolean(b);
+// console.log(bbool);
+
+// b = "" => false
+// b = "asd" => true
+
+//string Conversion
+
+let c = 33
+let stringC = String(c);
+console.log(c)
+console.log(typeof(stringC));
+
+// Number to string converted
