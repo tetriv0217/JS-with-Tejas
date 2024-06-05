b1bb2e1e1e5137ae54d726a7a890ebd123b80215
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-06-05 13:14:56 +0000

    Done with scopes and learn little bit about hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,9 +13,45 @@ if(true){
 // console.log(b);//Error
 // console.log(c);//30
 
-if(true){
-    let a = 100;
-    console.log("Inner A :- ", a);
+// if(true){
+//     let a = 100;
+//     console.log("Inner A :- ", a);
+// }
+// let a = 20;
+// console.log("Outer A :- ", a);
+
+
+function one(){
+    const username = "Tejas";
+    console.log(username);
+    function two() {
+        const website = "youtube";
+        console.log(username);//it can inherit from its parent function
+    }
+    two()
+    console.log(website);//This will show error as website is blocked scope
 }
-let a = 20;
-console.log("Outer A :- ", a);
+// one();//Tejas Tejas Error
+
+if(true){
+    const username = "hitesh";
+    if (true) {
+        const website = " youtube";
+        // console.log(username + website);//will print hitehs youtube
+    }
+    // console.log(website);//will show error
+}
+// console.log(username);//will show error
+
+
+//Hoisting 
+
+console.log(addone(5)); //will find addone function and result the desired output
+function addone(num) {
+    return num + 1;
+}
+
+addtwo(5) // This will show error as it is a variable and not able to intialise
+const addtwo = function(num) {
+    return num + 2;
+}
