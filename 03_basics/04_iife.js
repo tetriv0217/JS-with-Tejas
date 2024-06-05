cbf655690127f1a729c1868c408234e95c5bf2c6
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-06-05 16:44:33 +0000

    IIFE DONE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,21 @@
+//Immediately Invoked Function Expressions (IIFE)
+
+/*
+Why it is used:-
+1. To make it invoke immediately.
+2. To preseve it from global pollution i.e to save it global scope pollution.
+ */
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();
+//On arrow function
+
+
+
+
+// ()();
+// where first bracket is function definition 
+// where second bracket is function execution 
+
+// Semi colon is a must for IIFE
+
