1282fd555e610360a2ac3f96c646581f7eb698c0
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-06-16 00:46:37 +0000

    Done with Dom selector

---
+++ 06_dom/Notes/01_Dom.js
@@ -0,0 +1,48 @@
+/*
+
+Dom Fullform :- Document Object Model
+
+Window => Document => HTML
+HTML has two bodies: - Head,Body 
+  1.Title -> Title Node
+  2.Meta,Meta has attributes
+
+1.To get class we use className instead of class
+
+2.document.getElementById('className')
+
+3.innerText and textContent difference is that innerText gives the vivsible code while textContent give me the whole content.
+innerHTML gives the html as well
+
+4.query Selector all gives NodeList and its different from array.To select a different item,use[] and then apply the attributes.You can use forEach loop to iterate
+
+
+
+5.getElementsByClassName is a HTMLCollections and does not allow any looping.
+document.getElementsByClassName('list-item')
+  a)const tempClass = document.getElementsByClassName('list-item')
+  temp class is an html collection.
+  b) covert this into array using Array.form(HTMLCOLLECTION VARIABLE)
+  const covertedArray = Array.from(tempClass)
+  c)Do all the stuffs you want to do
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
