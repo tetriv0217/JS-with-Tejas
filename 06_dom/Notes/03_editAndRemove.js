8074a91b5c59085cfbb494e45e9ac6cf62c2ee54
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-06-17 13:21:40 +0000

    Done with adding and removing element

---
+++ 06_dom/Notes/03_editAndRemove.js
@@ -0,0 +1,11 @@
+/*
+//Edit best approach
+const newLi = document.createElement("li");
+newLi.textContent = "Mojo";
+secondLang.replaceWith(newLi);
+
+remove approach
+
+const lastLang = document.querySelector("li:last-child");
+lastLang.remove();
+*/
