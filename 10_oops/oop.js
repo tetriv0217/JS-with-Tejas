c43e65aa0cad58b4b929fb5fc7a277465e3766e6
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-06-23 07:11:17 +0000

    Done with opps in js

--- 10_oops/oop.js
+++ 10_oops/oop.js
@@ -18,17 +18,22 @@ function User(username,loginCount,signedIn){
     this.username = username;
     this.loginCount = loginCount;
     this.signedIn = signedIn;
+    this.greetings = function(){
+        console.log(`Welcome ${this.username}`);
+    }
     return this;
     // return this is implicit
 }
 //Use new keyword to create multiple instances or else userTwo will overide Userone
 const userOne = new User("tejas",4,true)
 const userTwo = new User("hitesh",17,false)
-console.log(userOne);
-console.log(userTwo);
-
+// console.log(userOne);
+// console.log(userTwo);
+userOne.greetings()
+userTwo.greetings()
 //new
 /**
- * new is used it creates a new instance
- * 
+ * 1. new is used it creates a new instance i.e empty object
+ * 2. constructor function is called after new keyword
+ * 3. all the arguments are returned in the function
  */
