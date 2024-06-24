2ddbcd19bac7a146c444b8799c4a673291f3e8ea
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2024-06-24 01:14:44 +0000

    decriptor properties

---
+++ 10_oops/mathpi.js
@@ -0,0 +1,26 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+// console.log(descripter);
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);//doesnt change since writable = false
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable:true,
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai,'name',{
+    // writable : false,
+    enumerable : false
+})
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (const [key,value] of Object.entries(chai)) {
+    if(typeof value != 'function'){
+        console.log(`${key} : ${value}`);//name will not get printed
+    }
+}
+
